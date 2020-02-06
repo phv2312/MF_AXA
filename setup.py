679986363184df077b9b7e7728463c9799cd3f0c
kan_cin <57749972+phv2312@users.noreply.github.com>
2020-02-06 12:27:47 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='mf_axa',
       description='Multiple Form AXA Handler',
       version='0.1.0',
 
-      packages=['mf_axa', 'mf_axa.clf', 'mf_axa.post_process'],
+      packages=['mf_axa', 'mf_axa.clf', 'mf_axa.post_process','mf_axa.clf.kan.form_type_clf'],
       install_requires=requirements,
       include_package_data=True,
       zip_safe=False)
