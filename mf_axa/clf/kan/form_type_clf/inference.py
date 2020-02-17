9ac8d5f9ed926b9ec8f7a218f8bdf562ccc37a58
kan <kat87yb@gmail.com>
2020-02-17 15:12:58 +0700

    update use_device mode

--- mf_axa/clf/kan/form_type_clf/inference.py
+++ mf_axa/clf/kan/form_type_clf/inference.py
@@ -9,7 +9,7 @@ import cv2
 import PIL.Image as Image
 
 class FormTypeClassifer:
-    def __init__(self, weight_path = ""):
+    def __init__(self, weight_path = "", device='cuda'):
         model_ft = resnet34(pretrained=True)
         num_ftrs = model_ft.fc.in_features
         # Here the size of each output sample is set to 2.
@@ -21,11 +21,20 @@ class FormTypeClassifer:
         self.model_ft = model_ft
         self.idx2cls = {0:'multi', 1:'single'}
 
+        assert device in ['cuda', 'cpu']
+        if device == 'cuda':
+            if not torch.cuda.is_available():
+                device = 'cpu'
+                print (">>> Can not use gpu, cpu instead !!!")
+
+        self.device = torch.device(device)
+        self.model_ft = self.model_ft.to(device)
+
     def process(self, np_image):
         pil_image = Image.fromarray(np_image)
         input_tensor = data_transforms['val'](pil_image) # (-> c,h,w)
 
-        input_tensor = input_tensor.unsqueeze(0) # -> (1,c,h,w)
+        input_tensor = input_tensor.unsqueeze(0).to(self.device) # -> (1,c,h,w)
         outputs = self.model_ft(input_tensor)
         _, preds = torch.max(outputs, 1) # _, (1,)
 
@@ -34,17 +43,13 @@ class FormTypeClassifer:
 
         return cls_preds
 
-
 if __name__ == '__main__':
-    clf = FormTypeClassifer(weight_path='/home/kan/Desktop/Cinnamon/mf/multiple_form/x/ahihi.ckpt')
+    clf = FormTypeClassifer(weight_path='/home/kan/Desktop/Cinnamon/mf/_form_type_weight.ckpt', device='cpu')
     import glob, os
 
     folder_dir = "/home/kan/Desktop/split_data (1)/ahihi/val/single"
     for im_fn in glob.glob(os.path.join(folder_dir, "*.tif")):
-
-        print (im_fn)
         sample_im = cv2.imread(im_fn)
 
-        clf.process(sample_im)
-
-    pass
+        _out = clf.process(sample_im)
+        print(im_fn, _out)
